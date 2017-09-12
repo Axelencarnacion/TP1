4503773cc0113bd995c1efa0b138657b246676a2
Axel <Axel@172.16.0.110>
2017-09-12 13:50:39 +0200

    Suite du dev

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="CSS/styles.css" ref="styleheet">
+    </head>
+    <body>
+        <h1>Ceci est le titre du TP1</h1>
+        <h2>Sous titre</h2>
+        <p>bla </p>
+        
+    </body>
+</html>
